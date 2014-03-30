b623d51b08fa1dfe660d2ddc7f582c3e71083e9e
Jeff Rabe <7livesleft@gmail.com>
2014-03-30 17:08:03 -0400

    Added login.html, and new servlet as a login endpoint. changed web.xml to reflect new servlet. new appengine xml has sessions enabled.

---
+++ src/cscie99/team2/lingolearn/client/LoginServlet.java
@@ -0,0 +1,61 @@
+package cscie99.team2.lingolearn.client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import cscie99.team2.lingolearn.server.UserServiceImpl;
+
+public class LoginServlet extends HttpServlet {
+
+	@Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, world");
+    }
+	
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+              throws IOException {
+    	
+    	try{
+	    	String gmail = req.getParameter("gmail") != null ?
+	    			req.getParameter("gmail") : "";
+	    	
+	    	String gplusId = req.getParameter("gplusid") != null ?
+	    			req.getParameter("gplusid") : "";
+	    		
+	    	
+	    	resp.setContentType("application/json");
+	    	// Get the printwriter object from response to write the required json object to the output stream      
+	    	PrintWriter out = resp.getWriter();
+	    	// Assuming your json object is **jsonObject**, perform the following, it will return your json object  
+	    	String json = "";
+	    			
+	    	if( gplusId.equals("") || gmail.equals("") ){
+	    		json = "false";
+	    	}else{
+	    	
+		    	HttpSession session = req.getSession();
+		    	session.setAttribute(
+		    			UserServiceImpl.GMAIL_SESSION_KEY, gmail );
+		    	session.setAttribute( UserServiceImpl.GID_SESSION_KEY,
+		    							gplusId );
+		    	json = "true";
+	    	}
+	    	
+	    	out.print(json);
+	    	out.flush();		
+	    	return;
+	    	
+    	}catch( NumberFormatException nfe ){
+    		nfe.printStackTrace();
+    		return;
+    	}
+    }
+}
