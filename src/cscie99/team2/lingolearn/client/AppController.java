9ddc5c348034202895f594b271f3284d054a0404
Jeff Rabe <7livesleft@gmail.com>
2014-03-30 18:40:25 -0400

    Changed the AppController class to determine if a user is logged in. This is currently stubbed -- i.e. you only need to be logged into gmail; you don't have to register yet.
    If you are not logged into gmail, you get redirecte to the login page.

--- src/cscie99/team2/lingolearn/client/AppController.java
+++ src/cscie99/team2/lingolearn/client/AppController.java
@@ -1,5 +1,13 @@
 package cscie99.team2.lingolearn.client;
 
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HasWidgets;
+
 import cscie99.team2.lingolearn.client.event.ViewCardEvent;
 import cscie99.team2.lingolearn.client.event.ViewCardEventHandler;
 import cscie99.team2.lingolearn.client.presenter.CardPresenter;
@@ -13,12 +21,7 @@ import cscie99.team2.lingolearn.client.view.CardView;
 import cscie99.team2.lingolearn.client.view.CourseView;
 import cscie99.team2.lingolearn.client.view.HomeView;
 import cscie99.team2.lingolearn.client.view.SessionView;
-
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.event.shared.HandlerManager;
-import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.ui.HasWidgets;
+import cscie99.team2.lingolearn.shared.Card;
 
 public class AppController implements Presenter, ValueChangeHandler<String> {
   private final HandlerManager eventBus;
@@ -27,6 +30,7 @@ public class AppController implements Presenter, ValueChangeHandler<String> {
   private final CourseServiceAsync courseService;
   private final AnalyticsServiceAsync analyticsService;
   private HasWidgets container;
+  private boolean userAuthenticated;
   
   public AppController(UserServiceAsync userService, CourseServiceAsync courseService,
 		  CardServiceAsync cardService, AnalyticsServiceAsync analyticsService, 
@@ -36,8 +40,10 @@ public class AppController implements Presenter, ValueChangeHandler<String> {
     this.cardService = cardService;
     this.courseService = courseService;
     this.analyticsService = analyticsService;
+    this.userAuthenticated = false;
     bind();
   }
+  
   
   private void bind() {
     History.addValueChangeHandler(this);
@@ -55,17 +61,34 @@ public class AppController implements Presenter, ValueChangeHandler<String> {
     History.newItem("viewCard");
   }
   
+  @Override
   public void go(final HasWidgets container) {
     this.container = container;
     
-    if ("".equals(History.getToken())) {
-      History.newItem("home");
-    }
-    else {
-      History.fireCurrentHistoryState();
-    }
+    authenticateUser();
   }
 
+  /*
+   * Redirect the User to their appropriate view.
+   * @param isAuthenticated - this indicates if the user is logged in
+   * or not.  If the user is not logged in, they will be
+   * redirected to the login page.
+   */
+  private void openView( boolean isAuthenticated ){
+
+	  if( isAuthenticated ){
+		  if ("".equals(History.getToken())) {
+		        History.newItem("home");
+		  }
+		  else {
+		        History.fireCurrentHistoryState();
+		  }
+	  }
+	  else {
+		  Window.Location.assign("/login.html");
+	  }
+  }
+  
   public void onValueChange(ValueChangeEvent<String> event) {
     String token = event.getValue();
     
@@ -101,4 +124,41 @@ public class AppController implements Presenter, ValueChangeHandler<String> {
       }
     }
   } 
+  
+  /*
+   * Check to see that a user is logged into this session.
+   * This is performed by calling the user service.
+   * One the async method returns, we can bring the user to a
+   * page (to use the app, or login).
+   *
+   * @notes this is stubbed.  Registration needs to be added
+   */
+  private void authenticateUser(){
+	  
+	  //TODO: Add registration redirection, rather than just
+	  // gmail login.
+	  /*
+	  userService.isUserLoggedIn( new AsyncCallback<Boolean>(){
+	  
+		  public void onSuccess(Boolean auth) {
+			  userAuthenticated = auth;
+		  }
+		      
+		  public void onFailure(Throwable caught) {
+		        Window.alert("Error fetching card.");
+		  }
+	  });
+	  */
+	  userService.getSessionGmail( new AsyncCallback<String>(){
+		  public void onSuccess(String gmail) {
+			  userAuthenticated = !gmail.equals("");
+			  openView(userAuthenticated);
+		  }
+		      
+		  public void onFailure(Throwable caught) {
+		        Window.alert("Error fetching card.");
+		  }
+	  
+	  });
+  }
 }
