4ab3599b9a89d40f6603ef8d26061f40010d2d19
rjzupkoii <rjzii@yahoo.com>
2014-04-01 14:04:53 -0400

    Create CardFileLoaderTest.java
    Stubbed out a unit test for the card loader.

---
+++ test/cscie99/team2/lingolearn/server/tools/CardFileLoaderTest.java
@@ -0,0 +1,23 @@
+package cscie99.team2.lingolearn.server.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test to make sure we can load card files to the data store and clean up afterwards.
+ */
+public class CardFileLoaderTest {
+  /**
+   * Test to make sure we can add cards to the data store.
+   */
+  @Test
+  public void testLoadFile() {
+    // Get a temporary file and export the TSV data to it
+    
+    // Create the loader and have it attempt to add the cards
+    
+    // Check the cards loaded against our reference set
+    
+  }
+}
