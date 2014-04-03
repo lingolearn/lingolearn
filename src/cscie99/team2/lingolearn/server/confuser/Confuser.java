0c6c4387fc2c54cf5e548b2ee84646e45f497fcb
rjzupkoii <rjzii@yahoo.com>
2014-04-03 10:14:50 -0400

    Update Confuser.java
    Stubbed out the functions for the confusers.

--- src/cscie99/team2/lingolearn/server/confuser/Confuser.java
+++ src/cscie99/team2/lingolearn/server/confuser/Confuser.java
@@ -1,9 +1,48 @@
 package cscie99.team2.lingolearn.server.confuser;
 
+import cscie99.team2.lingolearn.shared.Card;
+
 /**
  * This class encapsulates a means of getting characters that are similar to 
  * a given example for the purposes of confusing humans.
  */
 public class Confuser {
-
+  /**
+   * Get a list of kanji phrases that are similar to the one that has been
+   * provided. In the event that no kanji confusers for the given card then
+   * null will be returned, otherwise, confusers up to the provided count 
+   * will be provided.
+   */
+  public List<String> getKanjiPhrases(Card card, int count) {
+    // Get the confusers for Japanese
+    
+    // Break the card's kanji phrase appart so we have mulitple options
+    
+    // Iterate through the families until we find the correct one
+    
+      // We have a match, note the family
+      
+    // If we don't have any families identified, return null
+    
+    // Try to find a confuser based upon each family provided
+    
+    // Return the confusers
+    return null;
+  }
+  
+  /**
+   * Get a list of hiragana phrase confusers that are similar to the one
+   * that has been provided.
+   */
+  public List<String> getHiraganaPhrases(String phrase, int count) {
+    return null;
+  }
+  
+  /**
+   * Get a list of katakana phrase confusers that are similar to the one
+   * that has been provided.
+   */
+  public List<String> getKatakanaPhrases(String phrase, int count) {
+    return null;
+  }
 }
