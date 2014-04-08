066c3a279f2da7943a52f803ad7b7aa1f76d99d8
rjzupkoii <rjzii@yahoo.com>
2014-04-07 21:46:49 -0400

    Added code to test for bugs in getKanjiBoundries()

--- analysis/edu/harvard/analysis/ConfuserAnalysis.java
+++ analysis/edu/harvard/analysis/ConfuserAnalysis.java
@@ -8,6 +8,10 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import cscie99.team2.lingolearn.server.confuser.CharacterType;
+import cscie99.team2.lingolearn.server.confuser.Confuser;
+import cscie99.team2.lingolearn.server.confuser.ConfuserTools;
+import cscie99.team2.lingolearn.shared.Card;
 import cscie99.team2.lingolearn.shared.error.ConfuserException;
 
 /**
@@ -30,27 +34,80 @@ public class ConfuserAnalysis {
 	 * @throws IOException 
 	 */
 	public static void main(String [] args) throws IOException {
+		try {
+			List<Card> cards = loadCards(JLPT_DIRECTORY + JLPT_LEVEL + JLPT_EXTENSION);
+			analyzeConfusers(cards);
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open the JLPT level " + JLPT_LEVEL);
+		} catch (IOException ex) {
+			System.out.println("An IO exception occured.");
+			System.out.println(ex.toString());
+		} catch (ConfuserException ex) {
+			System.out.println("An error occured whlie processing the cards.");
+			System.out.println(ex.toString());
+		}
+	}
+	
+	/**
+	 * 
+	 * @param cards
+	 * @throws IOException 
+	 * @throws ConfuserException 
+	 */
+	private static void analyzeConfusers(List<Card> cards) throws ConfuserException, IOException {
+		// Prepare the counters
+		int kanji = 0, hiragana = 0, katakana = 0;
+		List<Integer> kanjiConfusers = new ArrayList<Integer>();
+		List<Integer> hiraganaConfusers = new ArrayList<Integer>();
+		List<Integer> katakanaConfusers = new ArrayList<Integer>();
+		// Iterate through the cards to find the results
+		Confuser confuser = new Confuser();
+		List<String> results;
+		for (Card card : cards) {
+			if (!card.getKanji().isEmpty()) {
+				try {
+					results = confuser.getKanjiBoundries(card);
+					results.addAll(confuser.getKanjiSubsitution(card.getKanji()));
+					kanji++;
+					kanjiConfusers.add(results.size());
+				} catch (ConfuserException ex) {
+					System.out.println(ex.getMessage());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param resourceName
+	 * @return
+	 */
+	private static List<Card> loadCards(String resourceName) throws IOException {
 		// Open the resource file
-		String path = JLPT_DIRECTORY + JLPT_LEVEL + JLPT_EXTENSION;
-		InputStream jlpt = ConfuserAnalysis.class.getResourceAsStream(path);
+		InputStream jlpt = ConfuserAnalysis.class.getResourceAsStream(resourceName);
 		// Prepare the stream reader
 		BufferedReader reader = null;
 		InputStreamReader stream = null;
+		List<Card> cards = new ArrayList<Card>();
 		try {
 			// Open up the stream to be read
 			stream = new InputStreamReader(jlpt);
 			reader = new BufferedReader(stream);
 			// Read and process the contents
-			List<String> results = new ArrayList<String>();
 			String data;
 			while ((data = reader.readLine()) != null) {
-				System.out.println(data);
+				// Parse the data and prepare the card
+				String[] values = data.split(",");
+				Card card = new Card();
+				card.setKanji(values[0]);
+				if (ConfuserTools.checkCharacter(values[1].charAt(0)) == CharacterType.Hiragana) {
+					card.setHiragana(values[1]);
+				} else {
+					card.setKatakana(values[1]);
+				}
+				card.setTranslation(values[2]);
+				cards.add(card);
 			}
-		} catch (FileNotFoundException ex) {
-			System.out.println("Unable to open the JLPT level " + JLPT_LEVEL);
-		} catch (IOException ex) {
-			System.out.println("An error occured while reading the next line");
-			System.out.println(ex.toString());
 		} finally {
 			if (reader != null) {
 				reader.close();
@@ -59,5 +116,7 @@ public class ConfuserAnalysis {
 				stream.close();
 			}
 		}
+		// Return the results
+		return cards;
 	}
 }
