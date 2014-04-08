5d144ed332097178cec81c4cf2f44f065ab3fad4
rjzupkoii <rjzii@yahoo.com>
2014-04-07 20:33:26 -0400

    Creation and initial commit.

---
+++ analysis/edu/harvard/analysis/ConfuserAnalysis.java
@@ -0,0 +1,11 @@
+package edu.harvard.analysis;
+
+/**
+ * This file is used to perform some basis analysis on the confuser algorithm
+ * to determine it's effectiveness.
+ */
+public class ConfuserAnalysis {
+	public static void main(String [] args) {
+		System.out.println("Testing");
+	}
+}
