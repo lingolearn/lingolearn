05af62ee6a8b274359a0f7d1a638fcced6896cd5
ypolyanskyy <yuriy.polyanskyy@gmail.com>
2014-04-07 20:25:22 -0400

    Added datastore support for UserSession and corresponding JUnit test.

---
+++ src/cscie99/team2/lingolearn/server/datastore/ObjectifyableUserSession.java
@@ -0,0 +1,56 @@
+/**
+ * CSCIE99 TEAM 2
+ */
+package cscie99.team2.lingolearn.server.datastore;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+import cscie99.team2.lingolearn.shared.UserSession;
+
+/**
+ * This class represents Proxy for User's Session
+ */
+@Entity(name="ObjectifyableUserSession")
+public class ObjectifyableUserSession implements Serializable {
+
+	private static final long serialVersionUID = -2526827390464962399L;
+
+	@Id private Long    userSessionId;	// Unique UserSession id
+	@Index private Long    sessionId;	// Session(Assignement) id
+	@Index private String  gplusId;      	// User id
+	private Date    sessStart,			// Timestamp of the session's start
+				    sessEnd;    		// Timestamp of the session's end
+
+	public ObjectifyableUserSession() {};
+	
+	/**
+	 * This method creates Objectifyable Proxy from real object
+	 * @param uSession		UserSession object
+	 */
+	public ObjectifyableUserSession(UserSession uSession) {
+		this.userSessionId = uSession.getUserSessionId();
+		this.sessionId = uSession.getSessionId();
+		this.gplusId = uSession.getGplusId();
+		this.sessStart = uSession.getSessStart();
+		this.sessEnd = uSession.getSessEnd();
+	};
+	
+	/**
+	 * This method reconstructs real object from Objectifyable Proxy
+	 * @return UserSession object
+	 */
+	public UserSession getUserSession() {
+		UserSession uSession = new UserSession();
+		uSession.setUserSessionId(this.userSessionId);
+		uSession.setSessionId(this.sessionId);
+		uSession.setGplusId(this.gplusId);
+		uSession.setSessStart(this.sessStart);
+		uSession.setSessEnd(this.sessEnd);
+		return uSession;
+	}
+}
