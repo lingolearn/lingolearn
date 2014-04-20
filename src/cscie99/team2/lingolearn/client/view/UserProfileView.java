5bc944a87c44386728391c80b04fabe1adc8d4f9
David Chouinard <david@davidchouinard.com>
2014-04-20 14:45:41 -0400

    Simplified profile view

--- src/cscie99/team2/lingolearn/client/view/UserProfileView.java
+++ src/cscie99/team2/lingolearn/client/view/UserProfileView.java
@@ -4,7 +4,10 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Widget;
 
 import cscie99.team2.lingolearn.shared.User;
@@ -15,42 +18,25 @@ public class UserProfileView extends Composite {
   private static final Binder binder = GWT.create(Binder.class);
 	
 	@UiField Element userName;
-	@UiField Element userInfo;
+	@UiField HTMLPanel userInfo;
 	
 	public UserProfileView(){
 		initWidget(binder.createAndBindUi(this));
 	}
 	
-  public Widget asWidget() {
-    return this;
-  }
-  
-  public void displayUserProfile( User user ){
-  	String fullName = user.getFirstName() + " " + user.getLastName();
-  	userName.setInnerHTML(fullName);
+	public Widget asWidget() {
+		return this;
+	}
+	  
+	public void displayUserProfile( User user ){
+		String fullName = user.getFirstName() + " " + user.getLastName();
+		userName.setInnerHTML(fullName);
   	
-  	StringBuilder buf = new StringBuilder();
-  	buf.append( getUserInfoElement("email ", user.getGmail()) );
-  	buf.append( getUserInfoElement("Native Language ", 
-  			user.getNativeLanguage().getLangName()) ); 
-  	buf.append( getUserInfoElement( "Gender", 
-  									user.getGender().toString()) );
-  	
-  	userInfo.setInnerHTML(buf.toString());
-  									
-  }
-	
-  private String getUserInfoElement( String label, String value ){
-  	String element = "<p class='user-profile-info'>"
-  							   + "<span class='user-info-label'>"
-  							   + label
-  							   + "</span>"
-  							   + "<span class='user-info-value'>"
-  							   + value
-  							   + "</span>"
-  							   + "</p>";
-  	
-  	return element;
-  }
+		userInfo.add(new Anchor(user.getGmail(), "mailto:" + user.getGmail()));
+		userInfo.add(new InlineLabel(" · "));
+		userInfo.add(new InlineLabel(user.getNativeLanguage().getLangName()));
+		userInfo.add(new InlineLabel(" · "));
+		userInfo.add(new InlineLabel(user.getGender().toString()));								
+	}
   
 }
