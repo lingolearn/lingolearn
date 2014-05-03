1dd10c1db663d8dee8a219edcf95d4179719a5a5
rjzupkoii <rjzii@yahoo.com>
2014-05-03 17:42:25 -0400

    Updated the enumeratoin to pretty print the output.

--- src/cscie99/team2/lingolearn/shared/SessionTypes.java
+++ src/cscie99/team2/lingolearn/shared/SessionTypes.java
@@ -1,13 +1,26 @@
 package cscie99.team2.lingolearn.shared;
 
 public enum SessionTypes {
-
-	Kanji_Translation, 
-	Hiragana_Translation,
-	Translation_Kanji,
-	Translation_Hiragana,
-	Kanji_Hiragana,
-	Hiragana_Kanji,
-	Confusor
+	Kanji_Translation("Kanji / Translation"), 
+	Hiragana_Translation("Hiragana / Translation"),
+	Translation_Kanji("Translation / Kanji"),
+	Translation_Hiragana("Translation / Hiragana"),
+	Kanji_Hiragana("Kanji / Hiragana"),
+	Hiragana_Kanji("Hiragana / Kanji"),
+	Confusor("Confusor");
 	
+	// The text returned by toString()
+	private String text;
+	
+	/**
+	 * Constructor, set the text for toString().
+	 */
+	private SessionTypes(String value) {
+		text = value;
+	}
+	
+	@Override
+	public String toString() {
+		return text;
+	}
 }
