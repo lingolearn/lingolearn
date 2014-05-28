d311dd3dd95b29b88a5eb42c692c7bc1fac38ada
rjzupkoii <rjzii@yahoo.com>
2014-05-27 22:13:39 -0400

    #53 More reformatting

--- src/cscie99/team2/lingolearn/client/presenter/SessionPresenter.java
+++ src/cscie99/team2/lingolearn/client/presenter/SessionPresenter.java
@@ -53,42 +53,36 @@ public class SessionPresenter implements Presenter {
 	private SpacedRepetition spacedRepetitionSystem;
 	private Course course;
 
-	public SessionPresenter(CourseServiceAsync courseService,
-			CardServiceAsync cardService, User currentUser,
+	public SessionPresenter(CourseServiceAsync courseService, CardServiceAsync cardService, User currentUser,
 			HandlerManager eventBus, SessionView display) {
 		this.courseService = courseService;
-		this.cardPresenter = new CardPresenter(cardService, eventBus,
-				new CardView());
-		this.quizPresenter = new QuizPresenter(cardService, eventBus,
-				new QuizView(), this);
+		this.cardPresenter = new CardPresenter(cardService, eventBus, new CardView());
+		this.quizPresenter = new QuizPresenter(cardService, eventBus, new QuizView(), this);
 		this.currentUser = currentUser;
 		this.eventBus = eventBus;
 		this.display = display;
 	}
 
 	public void bind() {
-
+		// Bind the basic event handlers
 		cardPresenter.getDisplay().getKnowledgeHighButton()
 				.addClickHandler(new ClickHandler() {
 					public void onClick(ClickEvent event) {
 						recordKnowledge(Assessment.DEFINITELYKNEWIT);
 					}
 				});
-
 		cardPresenter.getDisplay().getKnowledgeMediumButton()
 				.addClickHandler(new ClickHandler() {
 					public void onClick(ClickEvent event) {
 						recordKnowledge(Assessment.SORTAKNEWIT);
 					}
 				});
-
 		cardPresenter.getDisplay().getKnowledgeLowButton()
 				.addClickHandler(new ClickHandler() {
 					public void onClick(ClickEvent event) {
 						recordKnowledge(Assessment.NOCLUE);
 					}
 				});
-
 		quizPresenter.getDisplay().getNextButton()
 				.addClickHandler(new ClickHandler() {
 					public void onClick(ClickEvent event) {
@@ -96,36 +90,33 @@ public class SessionPresenter implements Presenter {
 					}
 				});
 
+		// Add support for keyboard events
 		Event.addNativePreviewHandler(new NativePreviewHandler() {
 			@Override
 			public void onPreviewNativeEvent(NativePreviewEvent event) {
 				NativeEvent ne = event.getNativeEvent();
 
 				if (event.getTypeInt() == Event.ONKEYDOWN) {
-					if (ne.getKeyCode() == KeyCodes.KEY_ENTER
-							|| ne.getKeyCode() == KeyCodes.KEY_SPACE) {
+					if (ne.getKeyCode() == KeyCodes.KEY_ENTER || ne.getKeyCode() == KeyCodes.KEY_SPACE) {
 						ne.preventDefault();
 						if (session instanceof Lesson) {
 							cardPresenter.flipCard();
 						} else {
-							if (quizPresenter.getDisplay().isAnswered())
+							if (quizPresenter.getDisplay().isAnswered()) {
 								gotoNextCard();
-							else
+							} else {
 								quizPresenter.submit();
+							}
 						}
 					}
-
 					if (session instanceof Lesson) {
-						if (ne.getKeyCode() == KeyCodes.KEY_ONE
-								|| ne.getKeyCode() == KeyCodes.KEY_NUM_ONE) {
+						if (ne.getKeyCode() == KeyCodes.KEY_ONE || ne.getKeyCode() == KeyCodes.KEY_NUM_ONE) {
 							ne.preventDefault();
 							recordKnowledge(Assessment.NOCLUE);
-						} else if (ne.getKeyCode() == KeyCodes.KEY_TWO
-								|| ne.getKeyCode() == KeyCodes.KEY_NUM_TWO) {
+						} else if (ne.getKeyCode() == KeyCodes.KEY_TWO || ne.getKeyCode() == KeyCodes.KEY_NUM_TWO) {
 							ne.preventDefault();
 							recordKnowledge(Assessment.SORTAKNEWIT);
-						} else if (ne.getKeyCode() == KeyCodes.KEY_THREE
-								|| ne.getKeyCode() == KeyCodes.KEY_NUM_THREE) {
+						} else if (ne.getKeyCode() == KeyCodes.KEY_THREE || ne.getKeyCode() == KeyCodes.KEY_NUM_THREE) {
 							ne.preventDefault();
 							recordKnowledge(Assessment.DEFINITELYKNEWIT);
 						}
@@ -135,8 +126,7 @@ public class SessionPresenter implements Presenter {
 								|| ne.getKeyCode() == KeyCodes.KEY_THREE
 								|| ne.getKeyCode() == KeyCodes.KEY_FOUR) {
 							ne.preventDefault();
-							quizPresenter.getDisplay().setSelectedAnswer(
-									ne.getKeyCode() - KeyCodes.KEY_ONE);
+							quizPresenter.getDisplay().setSelectedAnswer(ne.getKeyCode() - KeyCodes.KEY_ONE);
 						}
 
 					}
@@ -144,7 +134,6 @@ public class SessionPresenter implements Presenter {
 				}
 			}
 		});
-
 	}
 
 	public void go(final HasWidgets container) {
@@ -154,13 +143,11 @@ public class SessionPresenter implements Presenter {
 
 		try {
 			// Set session based on query parameter in URL
-			Long sessionId = (long) 0;
-
+			Long sessionId = 0l;
 			sessionId = Long.valueOf(Window.Location.getParameter("sessionId"));
 			this.setSession(sessionId);
 		} catch (NumberFormatException nfe) {
-			Notice.showNotice("The study session specified is invalid.",
-					"error");
+			Notice.showNotice("The study session specified is invalid.", "error");
 		}
 	}
 
@@ -168,62 +155,45 @@ public class SessionPresenter implements Presenter {
 	 * Sets and starts a session
 	 */
 	public void setSession(Long sessionId) {
-
 		courseService.getSessionById(sessionId, new AsyncCallback<Session>() {
 			public void onSuccess(Session returnedSession) {
 				session = returnedSession;
 
 				// Set "return to course" link of session
 				display.setReturnToCourseLink(returnedSession.getCourseId());
-
 				courseService.getCourseById(returnedSession.getCourseId(),
 						new AsyncCallback<Course>() {
 
 							@Override
 							public void onFailure(Throwable caught) {
-								Notice.showNotice(
-										"Unable to get course of session.",
-										"warning");
+								Notice.showNotice("Unable to get course of session.", "warning");
 							}
 
 							@Override
 							public void onSuccess(Course result) {
-
 								course = result;
-
 								if (course.getSpacedRepetitionOption() == SpacedRepetitionOption.LEITNER) {
 									spacedRepetitionSystem = new LeitnerSystem();
 								} else {
 									spacedRepetitionSystem = new BasicRandomization();
 								}
 
-								// Get the session type -- either from the
-								// query string, or from the quiz instance
-								// variable
-								// set by the instructor
-								String sessionType = Window.Location
-										.getParameter("type") == null ? ""
+								// Get the session type -- either from the query string, or from 
+								// the quiz instance variable set by the instructor
+								String sessionType = Window.Location.getParameter("type") == null ? ""
 										: Window.Location.getParameter("type");
 
 								SessionTypes type = SessionTypes.Kanji_Translation;
 								if (session instanceof Quiz) {
 									Quiz q = (Quiz) session;
 									type = q.getSessionType();
-									if (q.getMode().equals("yes")) {
-										quizPresenter.setUseConfusers(true,
-												q.getSessionType());
-									} else {
-										quizPresenter.setUseConfusers(false,
-												q.getSessionType());
-									}
+									boolean response = q.getMode().equals("yes");
+									quizPresenter.setUseConfusers(response, q.getSessionType());
 								} else {
 									try {
-										type = SessionTypes
-												.getEnum(sessionType);
+										type = SessionTypes.getEnum(sessionType);
 									} catch (IllegalArgumentException iae) {
-										Notice.showNotice(
-												"Unable to parse the session type ("
-														+ type + ")", "error");
+										Notice.showNotice("Unable to parse the session type (" + type + ")", "error");
 										return;
 									}
 								}
@@ -233,44 +203,29 @@ public class SessionPresenter implements Presenter {
 										currentUser.getGplusId(), type,
 										new AsyncCallback<UserSession>() {
 
-											public void onSuccess(
-													UserSession returnedUserSession) {
+											public void onSuccess(UserSession returnedUserSession) {
 												userSession = returnedUserSession;
-
 												if (session instanceof Lesson) {
-													cardPresenter.go(display
-															.getCardContainer());
+													cardPresenter.go(display.getCardContainer());
 												} else {
-													quizPresenter.go(display
-															.getCardContainer());
+													quizPresenter.go(display.getCardContainer());
 												}
-
-												display.setSessionName(session
-														.getDeck().getDesc());
-												spacedRepetitionSystem
-														.setDeck(session
-																.getDeck());
+												display.setSessionName(session.getDeck().getDesc());
+												spacedRepetitionSystem.setDeck(session.getDeck());
 												try {
-													spacedRepetitionSystem
-															.shuffleDeck();
+													spacedRepetitionSystem.shuffleDeck();
 												} catch (SpacedRepetitionException e) {
 													e.printStackTrace();
 												}
 												gotoNextCard();
 											}
 
-											public void onFailure(
-													Throwable caught) {
-												Window.alert("An unhandled error occured: "
-														+ caught.getMessage());
+											public void onFailure(Throwable caught) {
+												Window.alert("An unhandled error occured: " + caught.getMessage());
 											}
-
 										});
-
 							}
-
 						});
-
 			}
 
 			public void onFailure(Throwable caught) {
@@ -290,7 +245,6 @@ public class SessionPresenter implements Presenter {
 		flashCardResponse.setSeq(this.userSession.getSessionType().toString());
 		AnalyticsEvent flashCardEvent = new AnalyticsEvent(flashCardResponse);
 		eventBus.fireEvent(flashCardEvent);
-
 		// Move to the next card
 		gotoNextCard();
 	}
@@ -305,7 +259,6 @@ public class SessionPresenter implements Presenter {
 	}
 
 	public void gotoNextCard() {
-
 		boolean cardDrawn = true;
 		if (spacedRepetitionSystem.cardsRemaining()) {
 			try {
@@ -337,8 +290,7 @@ public class SessionPresenter implements Presenter {
 	private ArrayList<Long> selectThreeOtherCardsFromDeck() {
 		ArrayList<Long> selectedIds = new ArrayList<Long>();
 		List<Long> allIds = session.getDeck().getCardIds();
-		while ((selectedIds.size() < 3)
-				&& (selectedIds.size() != (allIds.size() - 1))) {
+		while ((selectedIds.size() < 3) && (selectedIds.size() != (allIds.size() - 1))) {
 			int idx = Random.nextInt(allIds.size());
 			boolean isValid = true;
 			for (int i = 0; i < selectedIds.size(); i++) {
